b39c3cca924b008422590973562cab6e35283ff1
minisabor90 <164228078+minisabor90@users.noreply.github.com>
2025-03-05 19:03:41 +0330

    Update Android.mk

--- curl/src/main/native/Android.mk
+++ curl/src/main/native/Android.mk
@@ -6,8 +6,7 @@ LOCAL_MODULE            := curl
 LOCAL_SRC_FILES         := $(addprefix curl/src/,$(CURL_CFILES))
 LOCAL_SRC_FILES         += $(addprefix curl/src/,$(CURLX_CFILES))
 LOCAL_SRC_FILES         += $(LOCAL_PATH)/tinynew.cpp
-LOCAL_C_INCLUDES        := $(LOCAL_PATH)/curl/lib
-LOCAL_C_INCLUDES        += $(LOCAL_PATH)/brotli/c/include
+LOCAL_C_INCLUDES        := $(LOCAL_PATH)/curl/lib $(LOCAL_PATH)/brotli/c/include
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
     LOCAL_C_INCLUDES    += $(LOCAL_PATH)/config32
 else ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
@@ -27,7 +26,7 @@ include $(CLEAR_VARS)
 include $(LOCAL_PATH)/curl/lib/Makefile.inc
 LOCAL_MODULE            := curl_static
 LOCAL_SRC_FILES         := $(addprefix curl/lib/,$(CSOURCES))
-LOCAL_C_INCLUDES        := $(LOCAL_PATH)/curl/include $(LOCAL_PATH)/curl/lib
+LOCAL_C_INCLUDES        := $(LOCAL_PATH)/curl/include $(LOCAL_PATH)/curl/lib  $(LOCAL_PATH)/brotli/c/include
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
     LOCAL_C_INCLUDES    += $(LOCAL_PATH)/config32
 else ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
