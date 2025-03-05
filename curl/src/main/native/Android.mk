c600362b71785fb040380d0491a02414fbd29a89
minisabor90 <164228078+minisabor90@users.noreply.github.com>
2025-03-05 17:37:45 +0330

    Update Android.mk

--- curl/src/main/native/Android.mk
+++ curl/src/main/native/Android.mk
@@ -40,8 +40,8 @@ else ifeq ($(TARGET_ARCH_ABI),riscv64)
 endif
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/curl/include
 LOCAL_EXPORT_LDLIBS     := -lz
-LOCAL_CFLAGS            := -DHAVE_CONFIG_H -DBUILDING_LIBCURL
-LOCAL_STATIC_LIBRARIES  := ssl_static nghttp2_static nghttp3_static ngtcp2_static
+LOCAL_CFLAGS            := -DHAVE_CONFIG_H -DBUILDING_LIBCURL -DUSE_BROTLI -DUSE_ZSD
+LOCAL_STATIC_LIBRARIES  := ssl_static nghttp2_static nghttp3_static ngtcp2_static brotli_static zsd_static
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -86,5 +86,29 @@ LOCAL_C_INCLUDES        += $(LOCAL_PATH)/config
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/nghttp3/lib/includes
 LOCAL_CFLAGS            := -DHAVE_CONFIG_H
 include $(BUILD_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+include $(LOCAL_PATH)/brotli/common/sources.mk
+include $(LOCAL_PATH)/brotli/dec/sources.mk
+include $(LOCAL_PATH)/brotli/enc/sources.mk
+LOCAL_MODULE            := brotli_static
+LOCAL_SRC_FILES         := $(addprefix brotli/common/,$(BROTLI_COMMON_C_SOURCES))
+LOCAL_SRC_FILES         += $(addprefix brotli/dec/,$(BROTLI_DEC_C_SOURCES))
+LOCAL_SRC_FILES         += $(addprefix brotli/enc/,$(BROTLI_ENC_C_SOURCES))
+LOCAL_C_INCLUDES        := $(LOCAL_PATH)/brotli/include
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/brotli/include
+LOCAL_CFLAGS            := -DBROTLI_BUILD_PORTABLE
+include $(BUILD_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := zsd_static
+LOCAL_SRC_FILES         := $(wildcard $(LOCAL_PATH)/zsd/src/*.c)
+LOCAL_C_INCLUDES        := $(LOCAL_PATH)/zsd/include
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/zsd/include
+LOCAL_CFLAGS            := -DHAVE_CONFIG_H
+include $(BUILD_STATIC_LIBRARY)
+
 
 $(call import-module,prefab/boringssl)
