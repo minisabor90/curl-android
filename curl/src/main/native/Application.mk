8e331d3b91e15092a4ac460d9bc88c33e68f2381
minisabor90 <164228078+minisabor90@users.noreply.github.com>
2025-04-10 02:49:42 +0330

    Update Application.mk

--- curl/src/main/native/Application.mk
+++ curl/src/main/native/Application.mk
@@ -1,6 +1,7 @@
 APP_CFLAGS     := -Wall -Wextra
 APP_CFLAGS     += -Werror -Wno-error=constant-logical-operand
 APP_CFLAGS     += -Wno-builtin-macro-redefined -D__FILE__=__FILE_NAME__
+APP_CFLAGS     += -Wno-error=unused-parameter
 APP_CONLYFLAGS := -std=c2x
 APP_LDFLAGS    := -Wl,--icf=all
 APP_STL        := none
